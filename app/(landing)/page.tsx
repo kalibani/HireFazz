495520100a5035d958fb9f04853b3eea79e9bce8
kalibani <kalibani.ka@gmail.com>
2023-07-30 12:53:48 +0700

    create a landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
-type Props = {};
-
-const LandingPage = (props: Props) => {
+const LandingPage = () => {
   return (
-    <div>
-      Landing Page
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
