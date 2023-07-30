f2c8d38b5621fe3bb823b9bfb3ad48d46e6ecc19
kalibani <kalibani.ka@gmail.com>
2023-07-30 13:02:24 +0700

    fix error build

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
+
+import { getAPILimitCount } from "@/lib/api-limit";
 import MobileSidebar from "./mobile-sidebar";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const apiLimitCount = await getAPILimitCount();
+
   return (
     <div className=" flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar apiLimitCount={apiLimitCount} />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
