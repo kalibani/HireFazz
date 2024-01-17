4b402b6c323717bd2925b15c66ae28b90a60342d
kalibani <kalibani.ka@gmail.com>
2024-01-17 10:02:36 +0700

    change order by

--- trpc/text-to-speech.ts
+++ trpc/text-to-speech.ts
@@ -87,6 +87,9 @@ const TextToSpeech = {
           where: {
             userId,
           },
+          orderBy: {
+            dateUnix: "desc",
+          },
           select: {
             id: true,
             text: true,
