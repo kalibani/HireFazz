432eb1478b010dbd25f901ec774d39989697752e
kalibani <kalibani.ka@gmail.com>
2024-01-17 09:59:16 +0700

    refactor TRPC

---
+++ trpc/document-interaction.ts
@@ -0,0 +1,136 @@
+import { privateProcedure } from "./trpc";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import prismadb from "@/lib/prismadb";
+import { MAX_FREE_COUNTS } from "@/constant";
+
+const DocumentInteraction = {
+  // get User Files
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    return await prismadb.file.findMany({
+      where: {
+        userId,
+      },
+    });
+  }),
+
+  // get file messages
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+      const limit = input.limit ?? MAX_FREE_COUNTS;
+
+      const file = await prismadb.file.findFirst({
+        where: {
+          id: fileId,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      const messages = await prismadb.message.findMany({
+        take: limit + 1,
+        where: {
+          fileId,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          isUserMessage: true,
+          createdAt: true,
+          text: true,
+        },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (messages.length > limit) {
+        const nextItem = messages.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      return {
+        messages,
+        nextCursor,
+      };
+    }),
+
+  // get upload file status
+  getFileUploadStatus: privateProcedure
+    .input(z.object({ fileId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const file = await prismadb.file.findFirst({
+        where: {
+          id: input.fileId,
+          userId: ctx.userId,
+        },
+      });
+
+      if (!file) return { status: "PENDING" as const };
+
+      return { status: file.uploadStatus };
+    }),
+
+  // get file
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await prismadb.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return file;
+    }),
+
+  // delete file
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await prismadb.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await prismadb.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
+};
+
+export const {
+  getFile,
+  getFileMessages,
+  getFileUploadStatus,
+  deleteFile,
+  getUserFiles,
+} = DocumentInteraction;
