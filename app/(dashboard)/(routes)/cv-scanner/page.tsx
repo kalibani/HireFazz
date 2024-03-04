037dc392fe6e0331c64548e9cee7019db8a6e7c8
dwicaksono <novan.top@gmail.com>
2024-03-04 16:57:59 +0700

    fix: card cv scanner

--- app/(dashboard)/(routes)/cv-scanner/page.tsx
+++ app/(dashboard)/(routes)/cv-scanner/page.tsx
@@ -43,6 +43,7 @@ import { usePricing } from "@/hooks/use-pricing";
 import { pricing } from "@/lib/utils";
 import toast from "react-hot-toast";
 import file from "react-player/file";
+import CardCvscanner from "@/components/card-cvscanner";
 
 // temporary set to 100 for testing purpose
 const limit = 100;
@@ -225,163 +226,15 @@ const CVAnalyzerPage = () => {
 				<div className="">
 					{/* @ts-ignore */}
 					{filesMemo && filesMemo?.length !== 0 ? (
-						<>
-							<ul className="grid grid-cols-1 gap-6 divide-y divide-zinc-200  xl:grid-cols-3 lg:grid-cols-2">
-								{/* @ts-ignore */}
-								{filesMemo
-									// @ts-ignore
-									.sort(
-										(
-											a: { reportOfAnalysis: { matchedPercentage: any } },
-											b: { reportOfAnalysis: { matchedPercentage: any } }
-										) =>
-											Number(b.reportOfAnalysis?.matchedPercentage) -
-											Number(a.reportOfAnalysis?.matchedPercentage)
-									)
-									.map((file: any) => (
-										<li
-											key={file.id}
-											className="col-span-1 transition bg-white divide-y divide-gray-200 rounded-lg shadow hover:shadow-lg">
-											<div className="flex items-center justify-between">
-												<Link
-													href={`/cv-scanner/${file.id}`}
-													className="flex flex-col flex-1 gap-2">
-													<div className="flex items-center justify-between max-w-[300px] p-4 space-x-2">
-														<div className="flex-shrink-0 w-10 h-10 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
-														<p className="text-lg font-medium line-clamp-1 text-zinc-900">
-															{file.name}
-														</p>
-													</div>
-												</Link>
-												{deletingIds.includes(file.id) ? (
-													<Loader2 className="mr-4 text-blue-500 animate-spin" />
-												) : (
-													<DropdownMenu>
-														<DropdownMenuTrigger asChild>
-															<Button
-																variant="ghost"
-																className="mr-4"
-																size="icon">
-																<MoreVertical />
-															</Button>
-														</DropdownMenuTrigger>
-														<DropdownMenuContent className="w-fit" align="end">
-															{!file.reportOfAnalysis && (
-																<DropdownMenuItem>
-																	<Button
-																		className="w-full "
-																		size="sm"
-																		onClick={() => setSelectedFile(file)}>
-																		Reanalyze
-																	</Button>
-																</DropdownMenuItem>
-															)}
-															<DropdownMenuItem>
-																<Button
-																	className="w-full text-red-500 border-red-500 hover:text-red-500"
-																	variant="outline"
-																	size="sm"
-																	onClick={() => handleDelete(file.id)}>
-																	Delete
-																</Button>
-															</DropdownMenuItem>
-														</DropdownMenuContent>
-													</DropdownMenu>
-												)}
-											</div>
-											<div>
-												<div className="flex lg:justify-between lg:flex-row flex-col-reverse px-4 pt-2 text-base text-zinc-900">
-													<div className="flex items-center gap-2">
-														{
-															<>
-																{/* @ts-ignore */}
-																{file.reportOfAnalysis ? (
-																	<>
-																		<p className="w-48 line-clamp-1">
-																			{/* @ts-ignore */}
-																			{file.reportOfAnalysis?.documentOwner ||
-																				file?.name}
-																		</p>
-																	</>
-																) : (
-																	<>
-																		<Plus className="w-4 h-4" />
-																		<span>
-																			{/* @ts-ignore */}
-																			{formatter.format(
-																				new Date(file.createdAt),
-																				"MMM yyyy"
-																			)}
-																		</span>
-																	</>
-																)}
-															</>
-														}
-													</div>
-
-													{(jobTitle && !file.reportOfAnalysis) ||
-													reanalyzeIds.includes(file.id) ? (
-														<div className="flex items-center p-2 text-base text-zinc-900">
-															Analyzing
-															<MoreHorizontal className="w-4 h-4 ml-1 opacity-50 shrink-0 animate-ping text-zinc-900" />
-														</div>
-													) : (
-														<>
-															{file.reportOfAnalysis &&
-															Object.keys(file.reportOfAnalysis).length > 0 ? (
-																<div className="flex lg:items-center lg:justify-center h-10 gap-1 text-base text-zinc-900">
-																	{/* @ts-ignore */}
-																	{file.reportOfAnalysis?.matchedPercentage}%
-																	<p>Match</p>
-																	{isMoreThanMatchLimit(
-																		// @ts-ignore
-																		file.reportOfAnalysis?.percentage,
-																		// @ts-ignore
-																		file.reportOfAnalysis?.matchedPercentage
-																	) ? (
-																		<Check className="w-5 h-5 text-green-500 " />
-																	) : (
-																		<X className="w-5 h-5 text-red-500 " />
-																	)}
-																</div>
-															) : (
-																<TooltipProvider>
-																	<label className="mr-2 text-lg font-semibold">
-																		Please Reanalyze
-																		<Tooltip delayDuration={300}>
-																			<TooltipTrigger className="cursor-default ml-1.5">
-																				<AlertCircle className="w-4 h-4 text-zinc-500" />
-																			</TooltipTrigger>
-																			<TooltipContent
-																				className="p-2 w-80"
-																				align="end">
-																				Process stopped because you refresh the
-																				page, please reanalyze and do not
-																				refresh the page
-																			</TooltipContent>
-																		</Tooltip>
-																	</label>
-																</TooltipProvider>
-															)}
-														</>
-													)}
-												</div>
-												<div className="px-4 pb-1 text-sm text-zinc-900">
-													{file.reportOfAnalysis?.jobTitle}
-												</div>
-												<div className="px-4 pb-4">
-													{!reanalyzeIds.includes(file.id) && (
-														<p className="text-xs text-zinc-500">
-															{/* @ts-ignore */}
-															{file.reportOfAnalysis?.reason}
-														</p>
-													)}
-												</div>
-											</div>
-										</li>
-									))}
-							</ul>
-						</>
+						<CardCvscanner
+							reanalyzeIds={reanalyzeIds}
+							deletingIds={deletingIds}
+							filesMemo={filesMemo}
+							isMoreThanMatchLimit={isMoreThanMatchLimit}
+							jobTitle={jobTitle}
+							onClickSelectFile={(val) => setSelectedFile(val)}
+							onDelete={(id) => deleteFile({ id })}
+						/>
 					) : isLoading ? (
 						<div className="flex items-start justify-center w-full p-8 rounded-lg bg-muted">
 							<LoaderGeneral />
--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -11,25 +11,25 @@ import { Button } from "@/components/ui/button";
 const font = Montserrat({ weight: "600", subsets: ["latin"] });
 
 export const LandingNavbar = () => {
-  const { isSignedIn } = useAuth();
+	const { isSignedIn } = useAuth();
 
-  return (
-    <nav className="p-4 bg-transparent flex items-center justify-between">
-      <Link href="/" className="flex items-center">
-        <div className="relative h-12 w-16 mr-4">
-          <Image fill alt="Logo" src="/BerryLabs.png" />
-        </div>
-        <h1 className={cn("text-2xl font-bold text-white", font.className)}>
-          BerryLabs
-        </h1>
-      </Link>
-      <div className="flex items-center gap-x-2">
-        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="outline" className="rounded-full">
-            Get Started
-          </Button>
-        </Link>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="p-4 bg-transparent flex items-center justify-between">
+			<Link href="/" className="flex items-center">
+				<div className="relative h-12 w-16 mr-4">
+					<Image fill alt="Logo" src="/BerryLabs.png" sizes="100%" />
+				</div>
+				<h1 className={cn("text-2xl font-bold text-white", font.className)}>
+					BerryLabs
+				</h1>
+			</Link>
+			<div className="flex items-center gap-x-2">
+				<Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+					<Button variant="outline" className="rounded-full">
+						Get Started
+					</Button>
+				</Link>
+			</div>
+		</nav>
+	);
 };
