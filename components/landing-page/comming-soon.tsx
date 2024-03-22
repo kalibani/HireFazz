3d2d0f4a00b45c8ed3e984f5beb0a649fad93332
dwicaksono <novan.top@gmail.com>
2024-03-22 15:17:06 +0700

    fix: adjust response and add footer and  mobile navogation

--- components/landing-page/comming-soon.tsx
+++ components/landing-page/comming-soon.tsx
@@ -5,15 +5,17 @@ import { WrapperSection } from '.';
 
 const CommingSoon = () => {
   return (
-    <WrapperSection className="flex h-screen flex-col items-center justify-center gap-y-12 ">
-      <h1 className="text-6xl font-semibold">Great things Comming Soon !</h1>
-      <p className="text-2xl">
+    <WrapperSection className="flex h-screen flex-col items-center justify-center  gap-y-9 lg:gap-y-12 ">
+      <h1 className="text-center text-5xl font-semibold lg:text-6xl">
+        Great things Comming Soon !
+      </h1>
+      <p className="px-3 text-center text-lg lg:text-2xl">
         We’re ready to build something new and more effecient to solving your
         repetitive task.
       </p>
-      <div className="flex w-full justify-center gap-x-4">
+      <div className="flex w-full justify-center gap-x-2 px-3 lg:gap-x-4">
         <Input
-          className="max-w-[288px]"
+          className="w-1/2 md:max-w-[288px]"
           inputMode="email"
           placeholder="email"
         />
@@ -21,7 +23,9 @@ const CommingSoon = () => {
           Notify Me
         </Button>
       </div>
-      <p className="text-2xl">Notify me when Feature is launched :)</p>
+      <p className="text-center text-lg lg:text-2xl">
+        Notify me when Feature is launched :)
+      </p>
     </WrapperSection>
   );
 };
