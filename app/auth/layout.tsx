900a90446120f1603bc4076e5e6e40f37725971b
dwicaksono <novan.top@gmail.com>
2024-04-02 21:00:24 +0700

    feat: layout login revamp

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+import logo from '@/public/icons/logo.svg';
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-primary via-primary to-sky-400">
-      {children}
+    <div className="h-full flex items-center justify-center ">
+      <div className="lg:h-[795px] flex items-center rounded-3xl shadow-xl lg:overflow-hidden">
+        <div className=" bg-rose-600 h-full lg:flex items-center p-36 w-1/2 justify-center hidden">
+          <Image alt="logo" src={logo} />
+        </div>
+        <div className="xl:px-32 lg:w-1/2 w-full flex justify-center">
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
