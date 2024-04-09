24bd10614b293b7b7d13ba896087a1dda91cf71c
dwicaksono <novan.top@gmail.com>
2024-04-09 22:35:40 +0700

    feat: change domain send email

--- lib/mail.ts
+++ lib/mail.ts
@@ -19,7 +19,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   const responseEmail = await resend.emails.send({
-    from: 'team@team.berrylabs.io',
+    from: 'team@berrylabs.io',
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
