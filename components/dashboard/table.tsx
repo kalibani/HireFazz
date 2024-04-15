ea88c27eeca7ffe2c4cf937e43cbd2d8d05f9e66
dimas wicaksono <novan.top@gmail.com>
2024-04-15 15:42:31 +0700

    feat:page dashboard onboard (#24)
    * feat:page dashboard onboard
    
    * feat: add table ui with sort each row
    
    * feat: revamp sidebar menu dashboard
    
    * change font from outfit to inter
    
    * Feat login (#23)
    
    * feat: login form and register
    
    * feat: changes clerk to authJS
    
    * core: add auth js and resend, also up version prisma
    
    * core: add public route
    
    * fix: change from clerk hook to auth custome hook to get userid
    
    * fix: disable cek verified login
    
    * fix: add config provider adapter
    
    * invite user flow
    
    * update db field
    
    * adjusting schema
    
    * refactore:login
    
    * feat: layout login revamp
    
    * feat: login form and register
    
    * feat: changes clerk to authJS
    
    * core: add auth js and resend, also up version prisma
    
    * core: add public route
    
    * fix: change from clerk hook to auth custome hook to get userid
    
    * fix: disable cek verified login
    
    * fix: add config provider adapter
    
    * invite user flow
    
    * update db field
    
    * adjusting schema
    
    * refactore:login
    
    * feat: layout login revamp
    
    * add conditional middleware if public route and islogin then direct to dashboard
    
    * fix:auth login and regis auto direct after login to dashboard
    
    * refactor: make it optimazie funct auth actions more easy to read and use
    
    * feat: change domain send email
    
    * feat: change domain send email
    
    ---------
    
    Co-authored-by: Riko Orlando <rikoorlando76@gmail.com>
    
    * feat:page dashboard onboard
    
    * feat: add table ui with sort each row
    
    * feat: revamp sidebar menu dashboard
    
    ---------
    
    Co-authored-by: Riko Orlando <rikoorlando76@gmail.com>

---
+++ components/dashboard/table.tsx
@@ -0,0 +1,241 @@
+'use client';
+
+import * as React from 'react';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { cn, formatDateStringToDate } from '@/lib/utils';
+
+const data: Payment[] = [
+  {
+    id: 'm5gr84i9',
+    listJob: 'Senior Software Engineer',
+    candidates: 143,
+    createdAt: '1712200652349',
+    status: 'Active',
+  },
+  {
+    id: '3u1reuv4',
+    listJob: 'Senior Software Engineer',
+    candidates: 20,
+    createdAt: '1712200652349',
+    status: 'Not Active',
+  },
+];
+
+export type Payment = {
+  id: string;
+  listJob: string;
+  status: 'Active' | 'Not Active';
+  createdAt: string;
+  candidates: number;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'listJob',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="w-fit px-4 pl-0 hover:bg-transparent"
+        >
+          List Job
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue('listJob')}</div>
+    ),
+  },
+  {
+    accessorKey: 'candidates',
+    header: ({ column }) => {
+      return (
+        <Button
+          className="w-fit px-4 pl-0 hover:bg-transparent"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Candidates
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <p className="capitalize text-slate-400">
+        {row.getValue('candidates')} candidates
+      </p>
+    ),
+  },
+  {
+    accessorKey: 'createdAt',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="w-fit px-4 pl-0 hover:bg-transparent"
+        >
+          Created at
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="font-normal capitalize text-slate-400">
+        {formatDateStringToDate(row.getValue('createdAt'))}
+      </div>
+    ),
+  },
+  {
+    accessorKey: 'status',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="w-fit px-4 pl-0 hover:bg-transparent"
+        >
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const status: string = row.getValue('status');
+      return (
+        <p
+          className={cn(
+            'text-sm font-normal capitalize',
+            status === 'Active' ? 'text-[#069A1E]' : 'text-primary'
+          )}
+        >
+          {status}
+        </p>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    cell: ({ row }) => {
+      return (
+        <Button variant="link" className="text-sm font-normal">
+          View Job
+        </Button>
+      );
+    },
+  },
+];
+
+const DashboardTable = () => {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="border">
+        <Table>
+          <TableHeader className="bg-slate-200">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      className="text-sm font-normal text-black"
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  // data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="py-0">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTable;
---
+++ components/share/banner.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+
+interface BannerProps {
+  src: string;
+  title: string;
+  desc: string;
+  btnTitle: string;
+}
+
+const Banner: FC<BannerProps> = ({ src, title, desc, btnTitle }) => {
+  return (
+    <div className="flex items-center justify-between rounded-lg bg-gradient-to-r from-rose-600 via-[#A24688] to-[#4E3ABA] px-11 py-4">
+      <div className="flex flex-col gap-y-4 text-white">
+        <h1 className="text-3xl font-semibold">{title}</h1>
+        <p className="max-w-[883px] text-sm">{desc}</p>
+        <Button className="w-fit text-sm font-medium" variant="secondary">
+          {btnTitle}
+        </Button>
+      </div>
+      <Image alt="icon" src={src} />
+    </div>
+  );
+};
+
+export default Banner;
