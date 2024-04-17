97c9f9cdad7aaa5401c48afaff8fa88307b16071
Riko Orlando <rikoorlando76@gmail.com>
2024-04-17 16:49:20 +0700

    fix create job and prettier

--- lib/actions/job/createJob.ts
+++ lib/actions/job/createJob.ts
@@ -1,9 +1,11 @@
+'use server';
+
 import { errorHandler } from '@/helpers';
 import prismadb from '@/lib/prismadb';
 import { WORK_MODEL } from '@prisma/client';
 import z from 'zod';
 import { uploadCv } from '../cv/uploadCv';
-
+import { v4 as uuidv4 } from 'uuid';
 const PayloadAddJob = z.object({
   jobName: z.string(),
   location: z.string(),
@@ -15,48 +17,55 @@ const PayloadAddJob = z.object({
   jobDescription: z.string(),
   companyName: z.string().optional(),
   orgId: z.string(),
-  analyzeCv: z.boolean()
+  analyzeCv: z.boolean(),
 });
 
-interface FileUpload {
-  id: string
-  cv: File
-}
-
-const connectCvJob = async ({cvId, orgId, batchJobId}: {cvId: string, orgId: string, batchJobId: string}) => {
+const connectCvJob = async ({
+  cvId,
+  orgId,
+  batchJobId,
+}: {
+  cvId: string;
+  orgId: string;
+  batchJobId: string;
+}) => {
   try {
     await prismadb.cvAnalysis.create({
       data: {
         cvId,
         orgId,
-        batchJobId,    
-      }
-    })
-    
+        batchJobId,
+      },
+    });
   } catch (error) {
     return errorHandler(error);
   }
-}
+};
 
 export const createJob = async (
   payload: z.infer<typeof PayloadAddJob>,
-  cv: FileUpload[]
+  cv: FormData,
 ) => {
   try {
-    // create Job
-    const { analyzeCv, ...safePayload} = PayloadAddJob.parse(payload)
+    const cvData = cv.getAll('files') as File[];
+    const generateObject = cvData.map((cv) => ({ cv, id: uuidv4() }));
+    const { analyzeCv, ...safePayload } = PayloadAddJob.parse(payload);
     const job = await prismadb.batchJob.create({
       data: {
         ...safePayload,
-      }
+      },
     });
-    const onSuccess = (props: {id: string, url: string, key: string}) => {
-      connectCvJob({cvId: props.id, orgId: safePayload.orgId, batchJobId: job.id})
-    }
-    cv.forEach(({id, cv}) => {
-      uploadCv({id, cv, source: "UPLOAD", orgId: payload.orgId}, onSuccess)
-    })
 
+    const onSuccess = (props: { id: string; url: string; key: string }) => {
+      connectCvJob({
+        cvId: props.id,
+        orgId: safePayload.orgId,
+        batchJobId: job.id,
+      });
+    };
+    generateObject.forEach(({ id, cv }) => {
+      uploadCv({ id, cv, source: 'UPLOAD', orgId: payload.orgId }, onSuccess);
+    });
   } catch (error) {
     return errorHandler(error);
   }
