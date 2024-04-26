5d14efb430c5c8506ec2e4dbd0fb7472345271dc
dwicaksono <novan.top@gmail.com>
2024-04-27 04:03:22 +0700

    fixed: move icon constant to page use client from constant because can make the server component page broken

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
 import dashboard from '@/public/icon/icon-banner-dashboard.svg';
 import { Banner, SectionWrap } from '@/components/share';
