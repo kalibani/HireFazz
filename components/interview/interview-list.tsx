84637693852a730f76fd8bfd3a0a748d764f89f3
dimas wicaksono <novan.top@gmail.com>
2024-05-13 11:48:42 +0700

    feat: slicing ui list template interview (#44)

--- components/interview/interview-list.tsx
+++ components/interview/interview-list.tsx
@@ -2,13 +2,42 @@
 
 import React from 'react';
 import { Button } from '../ui/button';
+import { Trash2, MoreVertical } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const InterviewList = ({ title }: { title: string }) => {
   return (
-    <div className="my-4 w-full rounded-lg border p-4 shadow">
-      <h4 className="text-lg font-bold">{title}</h4>
-      <Button className="mr-4">Delete</Button>
-      <Button>Edit</Button>
+    <div className="my-4 flex w-full items-center justify-between rounded-lg border p-4 shadow">
+      <div>
+        <h4 className="text-lg font-bold">{title}</h4>
+        <p>Job Name: {title}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+          <Trash2 className="size-5 text-primary" />
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            alignOffset={0}
+            className="flex flex-col gap-y-2 p-2"
+          >
+            <Button variant="ghost">Edit Question</Button>
+            <Button variant="ghost">Preview Question</Button>
+            <Button variant="ghost">Close Interview</Button>
+            <Button variant="ghost">Result</Button>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
