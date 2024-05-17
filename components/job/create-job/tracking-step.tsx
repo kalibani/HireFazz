30fa6dc95e523085d727cbb84f4c4f025e1eb904
Maulana Malik Y <67380302+maulanamaleek@users.noreply.github.com>
2024-05-17 18:13:38 +0700

    enhance ui of analyze cv page & modal (#54)

--- components/job/create-job/tracking-step.tsx
+++ components/job/create-job/tracking-step.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 type TitleMap = Record<number, { title: string; subTitle: string }>
 
-const TrackingStep = ({ step }: { step: number }) => {
+const TrackingStep = ({ step, withTitle = true }: { step: number; withTitle?: boolean }) => {
   const titleMap: TitleMap = {
     0: {
       title: 'Create New Job',
@@ -17,6 +17,10 @@ const TrackingStep = ({ step }: { step: number }) => {
       title: 'Upload CV',
       subTitle: 'Please add CV Candidates to add your job list.'
     },
+    3: {
+      title: 'CV Analyzer',
+      subTitle: 'Try to upload and analyze to see our magic'
+    }
   }
 
   const formTitle = titleMap[step]?.title || titleMap[0].title
@@ -25,30 +29,34 @@ const TrackingStep = ({ step }: { step: number }) => {
   return (
     <div className="flex items-center justify-between gap-x-2 rounded-md bg-white py-8 text-xs capitalize px-5">
       <div className="flex-1">
-        <h1 className="text-xl font-semibold">{formTitle}</h1>
-        <p>{formSubTitle}</p>
+        {withTitle && (
+          <>
+            <h1 className="text-xl font-semibold">{formTitle}</h1>
+            <p>{formSubTitle}</p>
+          </>
+        )}
       </div>
       <div className="flex-1 flex gap-2">
-      <p>Job Details</p>
-      <div className="flex items-center justify-center">
-        <div className="h-5 w-5 rounded-full bg-red-600" />
-        <div
-          className="h-[6px] w-80 bg-gray-200"
-          
-        >
-          <div className={cn(
-            'h-full w-1/2 bg-red-600',
-            step > 0 && 'w-full'
+        <p>Job Details</p>
+        <div className="flex items-center justify-center">
+          <div className="h-5 w-5 rounded-full bg-red-600" />
+          <div
+            className="h-[6px] w-80 bg-gray-200"
+
+          >
+            <div className={cn(
+              'h-full w-1/2 bg-red-600',
+              step > 0 && 'w-full'
             )} />
           </div>
-        <div
-          className={cn(
-            'h-5 w-5 rounded-full bg-gray-200 transition-all duration-300',
-            step > 0 && 'bg-red-600',
-          )}
-        />
-      </div>
-      <p>Job Description</p>
+          <div
+            className={cn(
+              'h-5 w-5 rounded-full bg-gray-200 transition-all duration-300',
+              step > 0 && 'bg-red-600',
+            )}
+          />
+        </div>
+        <p>Job Description</p>
       </div>
 
       <div className="flex-1"></div>
