6368584303c4d7a23b0d688152ca7fdbbb618083
dimas wicaksono <novan.top@gmail.com>
2024-06-15 16:41:06 +0700

    add dialog confirm (#94)
    you can use this hooks component for the confirmation action, like delete or change the data updated

---
+++ hooks/confirm/use-confirm.tsx
@@ -0,0 +1,55 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogHeader,
+  DialogTitle,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+} from '@/components/ui/dialog';
+import React, { useState } from 'react';
+
+export const useConfirm = (
+  title: string,
+  message: string,
+): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  const confirm = () =>
+    new Promise((resolve, reject) => {
+      setPromise({ resolve });
+    });
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+  const handleConfirm = () => {
+    promise?.resolve(true);
+    handleClose();
+  };
+  const handleCancel = () => {
+    promise?.resolve(false);
+    handleClose();
+  };
+
+  const ConfirmDialog = () => (
+    <Dialog open={promise != null}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{message}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="pt-2">
+          <Button variant="outline" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm}>Confirm</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+
+  return [ConfirmDialog, confirm];
+};
