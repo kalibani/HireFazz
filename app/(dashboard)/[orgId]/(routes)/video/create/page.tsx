d24dc856deb479d92bc00e87781cc928959d3fd9
dwicaksono <novan.top@gmail.com>
2024-06-18 19:31:33 +0700

    adjust video page template

--- app/(dashboard)/[orgId]/(routes)/video/create/page.tsx
+++ app/(dashboard)/[orgId]/(routes)/video/create/page.tsx
@@ -13,20 +13,11 @@ const page = async ({ params, searchParams }: ParamsProps) => {
         tagLine={`${searchParams?.id ? 'Edit' : 'Create'} detail Interview with question here`}
         isLabel
       />
-      <>
-        {/* <div className="rounded-md bg-white p-4">
-          <h3 className="text-2xl font-semibold">{`${searchParams?.id ? 'Edit' : 'Create'} Automatic Interview`}</h3>
-          <p className="text-sm font-normal text-slate-400">
-            {`${searchParams?.id ? 'Edit' : 'Create'} detail Interview with question here`}
-          </p>
-        </div> */}
-
-        <FormTemplate
-          orgId={params.orgId}
-          queryId={searchParams.id}
-          dataTemplate={searchParams.id ? dataTemplate : null}
-        />
-      </>
+      <FormTemplate
+        orgId={params.orgId}
+        queryId={searchParams.id}
+        dataTemplate={searchParams.id ? dataTemplate : null}
+      />
     </>
   );
 };
